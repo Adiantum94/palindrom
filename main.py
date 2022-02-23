1bfc34aebbc0b456dff2f9d3aed94dfbd9fdde00
Paulina Żurek <paucik94@gmail.com>
2022-02-23 16:44:37 +0100

    Add docstring

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def is_palindrome (word):
+    """
+        Check if the word is a palindrome, based on argument passed
+        Argument:
+        word
+    """    
     if word == word[::-1]:
         return True
     else:
